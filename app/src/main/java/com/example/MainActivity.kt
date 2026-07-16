4ec722234451856126c71391aefd714371ef8fd5
arifujjaman5569 <alifmdarifujjaman@gmail.com>
2026-07-16 16:35:20 +0600

    Test MainActivity

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,19 +1,20 @@
-package com.example
+package com.votmari.bloodfoundation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.ui.screens.MainAppContent
-import com.example.ui.theme.MyApplicationTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            MyApplicationTheme {
-                MainAppContent()
+            MaterialTheme {
+                Text("App Started Successfully")
             }
         }
     }
